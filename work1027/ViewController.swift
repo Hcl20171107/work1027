b5f6cb35cd1411b62ab3f7fb0b69e1d234e4e9e7
Hcl20171107 <s20171103185@admindeiMac-33.local>
2018-12-05 09:42:22 +0800

    计算器的完善
    增加了连加、连减、连乘、连除功能。

--- work1027/ViewController.swift
+++ work1027/ViewController.swift
@@ -10,83 +10,267 @@ import UIKit
 
 class ViewController: UIViewController {
     var temp:Double = 0
+    var ttemp:Double = 0
     var operatorFlag = 0
+    var re = 0
+    var judge = 0
+    var freguency = 0
+    var limit: Double = 0
+    var a = 0
     @IBOutlet weak var result: UITextField!
     @IBAction func button1(_ sender: Any) {
-        result.text = result.text! + "1"
+        if re == 1{
+            result.text = "1"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "1"
+        }
+        else{
+            result.text = result.text! + "1"
+        }
     }
     @IBAction func button2(_ sender: Any){
-        result.text = result.text! + "2"
+        if re == 1{
+            result.text = "2"
+            re = 0
+        }
+       else if a == 1{
+            result.text = ""
+            result.text = result.text! + "2"
+        }
+        else{
+            result.text = result.text! + "2"
+        }
     }
     @IBAction func button3(_ sender: Any) {
-        result.text = result.text! + "3"
+        if re == 1{
+            result.text = "3"
+            re = 0
+        }
+       else if a == 1{
+            result.text = ""
+            result.text = result.text! + "3"
+        }
+        else{
+            result.text = result.text! + "3"
+        }
     }
     @IBAction func button4(_ sender: Any) {
-        result.text = result.text! + "4"
+        if re == 1{
+            result.text = "4"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "4"
+        }
+        else{
+            result.text = result.text! + "4"
+        }
     }
     @IBAction func button5(_ sender: Any) {
-        result.text = result.text! + "5"
+        if re == 1{
+            result.text = "5"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "5"
+        }
+        else{
+            result.text = result.text! + "5"
+        }
     }
     @IBAction func button6(_ sender: Any) {
-        result.text = result.text! + "6"
+        if re == 1{
+            result.text = "6"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "6"
+        }
+        else{
+            result.text = result.text! + "6"
+        }
     }
     @IBAction func button7(_ sender: Any) {
-        result.text = result.text! + "7"
+        if re == 1{
+            result.text = "7"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "7"
+        }
+        else{
+            result.text = result.text! + "7"
+        }
     }
     @IBAction func button8(_ sender: Any) {
-        result.text = result.text! + "8"
+        if re == 1{
+            result.text = "8"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "8"
+        }
+        else{
+            result.text = result.text! + "8"
+            
+        }
     }
     @IBAction func button9(_ sender: Any) {
-        result.text = result.text! + "9"
+        if re == 1{
+            result.text = "9"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "9"
+        }
+        else{
+            result.text = result.text! + "9"
+        }
     }
     @IBAction func button0(_ sender: Any) {
-        result.text = result.text! + "0"
+        if re == 1{
+            result.text = "0"
+            re = 0
+        }
+        else if a == 1{
+            result.text = ""
+            result.text = result.text! + "0"
+        }
+        else{
+            result.text = result.text! + "0"
+        }
     }
     @IBAction func buttonPoint(_ sender: Any) {
-        result.text = result.text! + "."
+        if judge == 0{
+            if re == 1{
+                result.text = "0."
+                re = 0
+            }
+            else {
+                result.text = result.text! + "."
+            }
+            judge = 1
+        }
+        else{
+            result.text = result.text! + ""
+        }
     }
     @IBAction func buttonAdd(_ sender: Any) {
-        operatorFlag = 1
-        temp = Double(result.text!)!
-        result.text = ""
+        if result.text == ""{
+            result.text = "0"
+            re = 1
+        }
+        else if operatorFlag == 1{
+            temp = temp + Double(result.text!)!
+            result.text = "\(temp)"
+            a = 1
+        }
+        else{
+            operatorFlag = 1
+            temp = Double(result.text!)!
+            result.text = ""
+        }
     }
     @IBAction func buttonSub(_ sender: Any) {
-        operatorFlag = 2
-        temp = Double(result.text!)!
-        result.text = ""
+        if result.text == ""{
+            result.text = "0"
+            re = 1
+        }
+        else if operatorFlag == 2{
+            temp = temp - Double(result.text!)!
+            result.text = "\(temp)"
+            a = 1
+        }
+        else{
+            operatorFlag = 2
+            temp = Double(result.text!)!
+            result.text = ""
+        }
+       
     }
     @IBAction func buttonMult(_ sender: Any) {
-        operatorFlag = 3
-        temp = Double(result.text!)!
-        result.text = ""
+        if result.text == ""{
+            result.text = "0"
+            re = 1
+        }
+        else if operatorFlag == 3{
+            temp = temp * Double(result.text!)!
+            result.text = "\(temp)"
+            a = 1
+        }
+        else{
+            operatorFlag = 3
+            temp = Double(result.text!)!
+            result.text = ""
+        }
     }
     @IBAction func buttonDivi(_ sender: Any) {
-        operatorFlag = 4
-        temp = Double(result.text!)!
-        result.text = ""
+        if result.text == ""{
+            result.text = "0"
+            re = 1
+        }
+        else if operatorFlag == 4{
+            temp = temp / Double(result.text!)!
+            result.text = "\(temp)"
+            a = 1
+        }
+        else{
+            operatorFlag = 4
+            temp = Double(result.text!)!
+            result.text = ""
+        }
     }
 
     @IBAction func buttonAC(_ sender: Any) {
-        result.text = ""
+        result.text = "0"
+        re = 1
+        judge = 0
+        temp = 0
+        ttemp = 0
+        operatorFlag = 0
+        a = 0
     }
     @IBAction func buttonResult(_ sender: Any) {
-        if operatorFlag == 1{
-            temp = temp + Double(result.text!)!
-            result.text = "\(temp)"
+        if result.text == ""{
+            result.text = "0"
         }
-        if operatorFlag == 2{
-            temp = temp - Double(result.text!)!
-            result.text = "\(temp)"
+      //  else if a == 1{
+        //    temp = Double(result.text!)!
+          //  result.text = "\(temp)"
+        //}
+        else{
+            if operatorFlag == 1{
+                temp = temp + Double(result.text!)!
+                result.text = "\(temp)"
+            }
+            if operatorFlag == 2{
+                temp = temp - Double(result.text!)!
+                result.text = "\(temp)"
+            }
+            if operatorFlag == 3{
+                temp = temp * Double(result.text!)!
+                result.text = "\(temp)"
+            }
+            if operatorFlag == 4{
+                temp = temp / Double(result.text!)!
+                result.text = "\(temp)"
+            }
         }
-        if operatorFlag == 3{
-            temp = temp * Double(result.text!)!
-            result.text = "\(temp)"
-        }
-        if operatorFlag == 4{
-            temp = temp / Double(result.text!)!
-            result.text = "\(temp)"
-        }
-        
+        re = 1
+        judge = 0
+        temp = 0
+        ttemp = 0
+        operatorFlag = 0
+        a = 0
     }
     @IBAction func buttonMinus(_ sender: Any) {
             temp = Double(result.text!)!
@@ -95,16 +279,12 @@ class ViewController: UIViewController {
     }
     @IBAction func buttonDelete(_ sender: Any) {
         var ttemp: String = result.text!
-        let pp: Int = ttemp.characters.count - 1
-        if ttemp == ""
-        {
-            
+        for index in ttemp.indices{
+            print("\(ttemp[index])",terminator: "")
         }
-        else
-        {
-            ttemp = ttemp.substring(to:ttemp.index(ttemp.startIndex, offsetBy: pp))
-        }
-        result.text = ttemp
+        let range = ttemp.index(ttemp.endIndex, offsetBy: -1)..<ttemp.endIndex
+        ttemp.removeSubrange(range)
+        result.text = "\(ttemp)"
     }
     @IBAction func buttonSqrt(_ sender: Any) {
         temp = Double(result.text!)!
@@ -119,6 +299,11 @@ class ViewController: UIViewController {
     @IBAction func buttonCos(_ sender: Any) {
         temp = Double(result.text!)!
         temp = cos(Double(result.text!)!)
+        result.text = "\(temp)"
+    }
+    @IBAction func buttonPercent(_ sender: Any) {
+        temp = Double(result.text!)!
+        temp = Double(result.text!)! * 0.01
         result.text = "\(temp)"
     }
     override func viewDidLoad() {
